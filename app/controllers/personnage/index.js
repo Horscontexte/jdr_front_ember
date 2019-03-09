7affd93229bfb43823f408be9f123193c1dd892b
Camille-Dev <camille.theil@gmail.com>
2019-03-09 06:08:27 +0100

    patch: rename personnage.index controller action name

--- app/controllers/personnage/index.js
+++ app/controllers/personnage/index.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    transitionTo: function(item) {
+    transitionToPersonnage: function(item) {
       this.transitionToRoute('personnage.show', item.id);
     }
   }
