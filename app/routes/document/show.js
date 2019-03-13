5001534c362ec1bdaca8e81df818c321204bb80d
Camille-Dev <camille.theil@gmail.com>
2019-03-13 04:27:19 +0100

    patch: route for document/show is know the correct model

--- app/routes/document/show.js
+++ app/routes/document/show.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.store.findRecord('lieu', params.document_id);
+    return this.store.findRecord('document', params.document_id);
   }
 });
